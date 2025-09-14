f3136d30d5192be6349740fc02e5b86000b6fa6e
ru.gilmanov <ru.gilmanov@bars-open.ru>
2025-09-14 22:50:32 +0300

    Suka blyad

--- client-gui/build.rs
+++ client-gui/build.rs
@@ -2,9 +2,22 @@
 extern crate embed_resource;
 
 fn main() {
+    #[cfg(target_os = "windows")]
     println!("cargo:rustc-link-search=native=target\\debug");
+    #[cfg(target_os = "windows")]
     println!("cargo:rustc-link-search=native=target\\release");
+    #[cfg(target_os = "windows")]
     println!("cargo:rustc-link-search=native=target\\x86_64-pc-windows-gnu\\release");
+    #[cfg(target_os = "windows")]
+    println!("cargo:rustc-link-lib=dylib=tunnel_lib");
+
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
+    println!("cargo:rustc-link-search=native=target/debug");
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
+    println!("cargo:rustc-link-search=native=target/release");
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
+    println!("cargo:rustc-link-search=native=target/x86_64-pc-windows-gnu/release");
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
     println!("cargo:rustc-link-lib=dylib=tunnel_lib");
 
     #[cfg(target_os = "windows")]
